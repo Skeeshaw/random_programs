e523937fac29343cef017d32d98002849e4ccfc4
root <root@kali.skee>
2021-03-18 16:09:35 -0500

    added python bot startup code & private key hosted on apache2

--- discord_bot/javascript_bot/main.js
+++ discord_bot/javascript_bot/main.js
@@ -1,1 +1,11 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
 
+client.on('ready', () => {
+	console.log("Connected as " + client.user.tag)
+})
+
+//due to this file being public, my token is hosted privately with apache2
+bot_secret_token = "XXXXXX"
+
+client.login(bot_secret_token)
--- discord_bot/python_bot/main.py
+++ discord_bot/python_bot/main.py
@@ -1,6 +1,22 @@
-def main():
-    print("hey mark")
+import discord
+import urllib.request
 
+url = "http://192.168.1.146/password.txt"
+file = urllib.request.urlopen(url)
 
-main()
+for line in file:
+    decoded_line = line.decode("utf-8")
+    
 
+TOKEN = decoded_line
+
+client = discord.Client()
+
+@client.event
+async def on_ready(): # when bot starts
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('-------')
+
+client.run(TOKEN)
