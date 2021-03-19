09164c18e99ceff8610b7620f784c071b3370afb
root <root@kali.skee>
2021-03-18 19:53:05 -0500

    testing .env file for bot token privacy instead of using apache2

--- discord_bot/javascript_bot/main.js
+++ discord_bot/javascript_bot/main.js
@@ -1,11 +1,20 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
+const fs = require('fs')
+var fetch = require('node-fetch')
 
-client.on('ready', () => {
-	console.log("Connected as " + client.user.tag)
+
+//client.on('ready', () => {
+//	console.log("Connected as " + client.user.tag)
+//})
+
+fs.readFile('password.env', (err, data) => {
+	if (err) throw err;
+
+	var test = (data.toString());
+	console.log(test)
 })
 
-//due to this file being public, my token is hosted privately with apache2
-bot_secret_token = "XXXXXX"
 
-client.login(bot_secret_token)
+//due to this file being public, my token is being held in a .env file
+//client.login()
