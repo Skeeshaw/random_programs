3eab1f11562c5ad0a44b55ff26f581df6cdc847e
Brighton Hilyard <36248386+hilyardbrighton@users.noreply.github.com>
2022-09-28 20:49:26 -0500

    got the code working

--- c++/Chapter4/main.cpp
+++ c++/Chapter4/main.cpp
@@ -2,28 +2,35 @@
 #include <string>
 using namespace std;
 
-class MyClass {       // The class
-  public:             // Access specifier
-    int total;        
-};
+
+int main() {
+  //until i find a better way of running functions individually i will just change this line
+  //every time i want to run a different one
+  bug_collector();
+  return 0;
+}
+
 void bug_collector() {
 
   cout << "Welcome to Bug Masters bug collection system." << "\n";
 
+  //idk why but if i dont set this to 0 it gives me a weird output
+  int total = 0;
+  
+  int userinput;
+  
   //in c, the accumulator can be set in the for loop conditions
   //this is the same as "for num in range(1,6)"
   for (int accum = 1;accum < 6;accum++) {
-    int userinput;
+    
+    
     cout << "\nHow many bugs did you collect on day " << accum << "? ";
     cin >> userinput;
 
-    int total = total + userinput;
+    total = total + userinput;
     
   }
-  cout << "Great job, you collected " << total << " bugs this week. You're the bug master!";
+  
+  cout << "\nGreat job, you collected " << total << " bugs this week. You're the bug master!";
 }
 
-int main() {
-  bug_collector();
-  return 0;
-}
