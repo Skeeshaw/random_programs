6e878d805736314a1cac243b2c59eebf68a2e9c1
BrightonHilyard <36248386+hilyardbrighton@users.noreply.github.com>
2022-10-06 14:52:20 -0500

    cigar

--- python/wordle/wordletest.py
+++ python/wordle/wordletest.py
@@ -1,4 +1,5 @@
 import random
+from colorama import Fore, Style
 
 first_row = ["[ ]","[ ]","[ ]","[ ]","[ ]"]
 second_row = ["[ ]","[ ]","[ ]","[ ]","[ ]"]
@@ -19,6 +20,7 @@ answer = content[ansnum]
 answerfile.close()
 
 
+# ------------ input section ------------ #
 
 guessfile = open("guesses.txt","r")
 
@@ -34,3 +36,79 @@ while good:
     else:
         good = False
 
+
+
+#making sure word actually exists
+exists = False
+line = guessfile.readline()
+
+while line != '':
+    
+    line = line.rstrip("\n")
+    
+    if line.lower() == choice.lower():
+        exists = True
+        
+    line = guessfile.readline()
+    
+    
+
+if not exists:
+    print("Your word was not recognized as a correct input. Please try again.")
+    
+    
+choicelist = [*choice]
+anslist = [*answer]
+
+anslist.pop()
+
+print(anslist)
+
+#checking if a common letter exists
+commonletter = False
+
+def itemchecker(list1,list2):
+    
+    itemlist = []
+    
+    for item in list1:
+        
+        if item in list2:
+            
+            commonletter = True
+            itemlist.append(item)
+            
+            place_in_choice = list1.index(item)
+            
+            place_in_ans = list2.index(item)
+            
+    return itemlist, place_in_ans, place_in_choice
+
+itemlist, place_in_ans, place_in_choice = itemchecker(choicelist,anslist)
+
+print(itemlist)
+        
+if commonletter:
+    if place_in_choice == place_in_ans:
+        #common letter exists & is same placement
+        sameplace = True
+    else:
+        #common letter exists but not the same placement
+        sameplace = False
+        
+for amoung in range(5):
+    
+    #print(Fore.RED + <string>)
+    #print(Style.RESET_ALL + <string>)
+    
+    if amoung == place_in_ans:
+        first_row[amoung] = (Fore.YELLOW + str("[" + choice[amoung] + "]"))
+        
+    elif place_in_choice == place_in_ans:
+        
+        first_row[amoung] = (Fore.GREEN + str("[" + choice[amoung] + "]"))
+        
+    else:
+        first_row[amoung] = "[" + choice[amoung] + "]"
+    
+print(first_row)
